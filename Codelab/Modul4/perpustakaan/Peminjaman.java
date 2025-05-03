5a7bcb4650f6eb2ff431306541659f57b6fda451
Dianoctavian-003 <octaviandian17@gmail.com>
2025-05-03 17:03:24 +0700

    First commit

---
+++ Codelab/Modul4/perpustakaan/Peminjaman.java
@@ -0,0 +1,9 @@
+package Codelab.Modul4.perpustakaan;
+
+public interface Peminjaman {
+    void pinjamBuku(String judul);
+
+    void pinjamBuku(String judul, int durasi);
+
+    void kembalikanBuku(String judul);
+}
