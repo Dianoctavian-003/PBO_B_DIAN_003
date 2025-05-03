ca4121252953445eae959dc4cd290067ed8deb94
Dianoctavian-003 <octaviandian17@gmail.com>
2025-05-03 17:04:22 +0700

    First commit

---
+++ Tugas/Modul3/Mahasiswa.java
@@ -0,0 +1,23 @@
+package Tugas.Modul3;
+
+public class Mahasiswa extends User{
+    // Constructor menggunakan supe
+
+    public Mahasiswa(String nama, String nim) {
+        super(nama, nim);
+    }
+
+    // Override method login
+    @Override
+    public boolean login(String inputNama, String inputNim) {
+        return getNama().equals(inputNama) && getNim().equals(inputNim);
+    }
+
+    // Override method displayInfo
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Role: Mahasiswa");
+        System.out.println("Login berhasil sebagai Mahasiswa!");
+    }
+}
